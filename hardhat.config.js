9c5849a6e59fcd0a44852104b83aa9125441a905
Mzterwalexxzy <kareemtoheeba10@gmail.com>
2024-08-08 03:21:55 +0000

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+nano hardhat.config.js
+nano hardhat.config.js
+nano hardhat.config.js
+nano hardhat.config.js
+nano hardhat.config.js
+nano hardhat.config.js
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: ["0ae"],
+    },
+  },
+};
